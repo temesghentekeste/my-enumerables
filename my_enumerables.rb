6646ed7b45a8e6931944405634d839274d116565
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-06-10 20:29:37 +0300

    Added logic for my_select enumberable method inside my_enumerable.rb file - Added logic to test the functionality of my_select method using main.rb file

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -33,4 +33,21 @@ module Enumerable
     end
     self
   end
+
+  def my_select 
+    return enum_for(:my_select) unless block_given?
+    result = []
+    hash_result = {}
+    i = 0
+    while i < self.size
+        if is_a?(Array) || is_a?(Range)
+            result << self[i] if yield(self[i])
+        elsif is_a?(Hash)
+            hash_result[keys[i]] = self[keys[i]] if yield(keys[i],self[keys[i]])
+        end
+        i+=1
+    end
+    return result if result.size > 0
+    return hash_result if hash_result.size > 0
+  end
 end
