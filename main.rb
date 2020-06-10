f3852f1258a1e14448f081388e5e03a92aed38dc
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-06-10 18:51:07 +0300

    Added logic for my_each_with_index enumerable method in my_enumerables.rb file - Added logic to test the functionality of the my_each_with_index method in main.rb file

--- main.rb
+++ main.rb
@@ -1,12 +1,12 @@
 require_relative('my_enumerables')
 
-# puts "Output: Array"
+################################## my_each ###################################
+# puts "my_each Output: Array"
 # array = [2,3,4]
 # p array.each {|x| print "#{x}\t"}
 # p array.my_each {|x| print "#{x}\t"}
 
-
-# puts "Output: Hash"
+# puts "my_each Output: Hash"
 # require_relative('my_enumerables')
 # countries_code = {
 #     eritrea: 291,
@@ -17,7 +17,34 @@ require_relative('my_enumerables')
 # p countries_code.each {|x| p x}
 # p countries_code.my_each {|x| p x}
 
-
-# puts "Output: Range"
+# puts "my_each Output: Range"
 # p (0..10).each {|x| print "#{x}\t"}
 # p (0..10).my_each {|x| print "#{x}\t"}
+
+################################## my_each_with_index ###################################
+
+# puts "my_each_with_index Output: Array"
+# array = [2,3,4]
+# p array.each_with_index {|x, y| print "#{x}::#{y}\t"}
+# p array.my_each_with_index {|x, y| print "#{x}::#{y}\t"}
+
+# puts "my_each_with_index Output: Hash"
+# require_relative('my_enumerables')
+# countries_code = {
+#     eritrea: 291,
+#     nigeria: 234,
+#     uganda: 256
+# }
+
+# p (countries_code.each_with_index do |x, y|
+#     puts "#{x}, #{y}"
+# end)
+
+# p (countries_code.my_each_with_index do |x, y|
+#     puts "#{x}, #{y}"
+# end)
+
+# puts 'my_each_with_index Output: Range'
+# p(0..10).each_with_index { |x, y| print "#{x}::#{y}\t" }
+# puts
+# p(0..10).my_each_with_index { |x, y| print "#{x}::#{y}\t" }
--- my_enumerables.rb
+++ my_enumerables.rb
@@ -1,20 +1,36 @@
 module Enumerable
-    def my_each
-        if block_given?
-            index = 0
-            while index < self.size
-                if is_a?(Array)
-                    yield(self[index])
-                elsif is_a?(Range)
-                    yield(self.to_a[index])
-                elsif is_a?(Hash)
-                    yield([keys[index], self[keys[index]]])
-                end
-                index += 1
-            end
-        else
-            return enum_for(:my_each)
-        end
-        self
+  def my_each
+    return enum_for(:my_each) unless block_given?
+
+    index = 0
+    while index < size
+      if is_a?(Array)
+        yield(self[index])
+      elsif is_a?(Range)
+        yield(to_a[index])
+      elsif is_a?(Hash)
+        yield([keys[index], self[keys[index]]])
+      end
+      index += 1
     end
+
+    self
+  end
+
+  def my_each_with_index
+    return enum_for(:my_each_with_index) unless block_given?
+
+    index = 0
+    while index < size
+      if is_a?(Array)
+        yield(self[index], index)
+      elsif is_a?(Range)
+        yield(to_a[index], index)
+      elsif is_a?(Hash)
+        yield([keys[index], self[keys[index]]], index)
+      end
+      index += 1
+    end
+    self
+  end
 end
