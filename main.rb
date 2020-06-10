b3c7631273cf276b71f3063ef5c1c24cdc1cd318
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-06-10 18:21:40 +0300

    Added Logic to my_enumerables.rb file for my_each enumberable method

---
+++ main.rb
@@ -0,0 +1,23 @@
+require_relative('my_enumerables')
+
+# puts "Output: Array"
+# array = [2,3,4]
+# p array.each {|x| print "#{x}\t"}
+# p array.my_each {|x| print "#{x}\t"}
+
+
+# puts "Output: Hash"
+# require_relative('my_enumerables')
+# countries_code = {
+#     eritrea: 291,
+#     nigeria: 234,
+#     uganda: 256
+# }
+
+# p countries_code.each {|x| p x}
+# p countries_code.my_each {|x| p x}
+
+
+# puts "Output: Range"
+# p (0..10).each {|x| print "#{x}\t"}
+# p (0..10).my_each {|x| print "#{x}\t"}
--- my_enumerables.rb
+++ my_enumerables.rb
@@ -3,11 +3,18 @@ module Enumerable
         if block_given?
             index = 0
             while index < self.size
-                
+                if is_a?(Array)
+                    yield(self[index])
+                elsif is_a?(Range)
+                    yield(self.to_a[index])
+                elsif is_a?(Hash)
+                    yield([keys[index], self[keys[index]]])
+                end
                 index += 1
             end
         else
             return enum_for(:my_each)
         end
+        self
     end
 end
